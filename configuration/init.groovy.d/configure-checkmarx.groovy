c00ef1fa8fa920d538366fe08ef6d5fa1e16e73b
Zhao Li <zhao.li@startingspark.com>
2020-05-07 17:07:19 -1000

    add initialization script structure

---
+++ configuration/init.groovy.d/configure-checkmarx.groovy
@@ -0,0 +1,21 @@
+import jenkins.model.Jenkins
+import java.util.logging.Level
+import java.util.logging.Logger
+
+final def LOG = Logger.getLogger("LABS")
+
+def checkmarxBaseUrl = System.getenv('SLACK_BASE_URL') // TODO: update
+
+if(checkmarxBaseUrl != null) {
+  LOG.log(Level.INFO,  'Configuring checkmarx...' )
+
+  def checkmarxCredentialId = "jenkins-git-password" // TODO: update
+
+  def checkmarx = Jenkins.instance.getDescriptorByType(com.checkmarx.jenkins.CxScanBuilder.DescriptorImpl)
+
+  checkmarx.baseUrl = checkmarxBaseUrl
+  checkmarx.tokenCredentialId = checkmarxCredentialId ? System.getenv('OPENSHIFT_BUILD_NAMESPACE') + "-" + checkmarxCredentialId : ''
+  checkmarx.save()
+
+  LOG.log(Level.INFO,  'Configured checkmarx' )
+}
